48a99d992a540dbab193799744d2d83d669eda64
Jassem Al-Buloushi <jassem1988@gmail.com>
2019-04-17 08:52:32 +0300

    Edit the labels spelling and add withAnchor to labels and topAnchor to the previous label and add safeSpace

--- AutoLayout-withCode/ViewController.swift
+++ AutoLayout-withCode/ViewController.swift
@@ -13,51 +13,66 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let lable1 = UILabel()
-        lable1.translatesAutoresizingMaskIntoConstraints = false  //To make our own contsraints
-        lable1.backgroundColor = .red
-        lable1.text = "THESE"
-        lable1.sizeToFit()
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false  //To make our own contsraints
+        label1.backgroundColor = .red
+        label1.text = "THESE"
+        label1.sizeToFit()
         
-        let lable2 = UILabel()
-        lable2.translatesAutoresizingMaskIntoConstraints = false
-        lable2.backgroundColor = .cyan
-        lable2.text = "ARE"
-        lable2.sizeToFit()
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = .cyan
+        label2.text = "ARE"
+        label2.sizeToFit()
         
-        let lable3 = UILabel()
-        lable3.translatesAutoresizingMaskIntoConstraints = false
-        lable3.backgroundColor = .yellow
-        lable3.text = "SOME"
-        lable3.sizeToFit()
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = .yellow
+        label3.text = "SOME"
+        label3.sizeToFit()
         
-        let lable4 = UILabel()
-        lable4.translatesAutoresizingMaskIntoConstraints = false
-        lable4.backgroundColor = .green
-        lable4.text = "AWESOME"
-        lable4.sizeToFit()
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = .green
+        label4.text = "AWESOME"
+        label4.sizeToFit()
         
-        let lable5 = UILabel()
-        lable5.translatesAutoresizingMaskIntoConstraints = false
-        lable5.backgroundColor = .orange
-        lable5.text = "LABELS"
-        lable5.sizeToFit()
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = .orange
+        label5.text = "LABELS"
+        label5.sizeToFit()
         
-        view.addSubview(lable1)
-        view.addSubview(lable2)
-        view.addSubview(lable3)
-        view.addSubview(lable4)
-        view.addSubview(lable5)
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
         
-        let viewsDictionary = ["label1": lable1, "label2": lable2, "label3": lable3, "label4": lable4, "label5": lable5]
+//        let viewsDictionary = ["label1": lable1, "label2": lable2, "label3": lable3, "label4": lable4, "label5": lable5]
+//
+//        for label in viewsDictionary.keys {
+//            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        }
+//
+//        let metrics = ["labelHeight": 88]
+//
+//        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
         
-        for label in viewsDictionary.keys {
-            view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+        var previous: UILabel?
+        
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            
+            if let previous = previous {
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            } else {
+                label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+            }
+            
+            previous = label
         }
-        
-        let metrics = ["labelHeight": 88]
-        
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
     }
 
 
